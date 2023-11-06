0cbf96f32dab2eafa6a21b3c5f4b0a774bd2d267
Michael Jiménez <micjejim@espol.edu.ec>
2023-11-06 10:56:06 -0500

    clase

--- public/javascript/load_data.js
+++ public/javascript/load_data.js
@@ -40,7 +40,6 @@ let cargarPrecipitacion = () => {
   precipitacionMaxValue.textContent = `Max ${max} [mm]`;
 };
 
-
 let cargarFechaActual = () => {
   //Obtenga la referencia al elemento h6
   //Obtenga la referencia al elemento h6
@@ -51,10 +50,10 @@ let cargarFechaActual = () => {
   tituloH6.textContent = fechaActual();
 };
 
-
 let cargarOpenMeteo = () => {
   //URL que responde con la respuesta a cargar
-  let URL = "https://api.open-meteo.com/v1/forecast?latitude=-2.1962&longitude=-79.8862&hourly=temperature_2m&timezone=auto";
+  let URL =
+    "https://api.open-meteo.com/v1/forecast?latitude=-2.1962&longitude=-79.8862&hourly=temperature_2m&timezone=auto";
 
   fetch(URL)
     .then((responseText) => responseText.json())
@@ -91,54 +90,135 @@ let cargarOpenMeteo = () => {
     .catch(console.error);
 };
 let cargarOpenMeteo2 = () => {
-    //URL que responde con la respuesta a cargar
-    let URL = "https://api.open-meteo.com/v1/forecast?latitude=-2.1962&longitude=-79.8862&hourly=relativehumidity_2m,apparent_temperature,precipitation_probability&timezone=auto";
-  
-    fetch(URL)
-      .then((responseText) => responseText.json())
-      .then((responseJSON) => {
-        console.log(responseJSON);
-  
-        //Respuesta en formato JSON
-        //Referencia al elemento con el identificador plot
-        let plotRef = document.getElementById("plot2");
-  
-        //Etiquetas del gráfico
-        let labels = responseJSON.hourly.time;
-  
-        //Etiquetas de los datos
-        let data1 = responseJSON.hourly.relativehumidity_2m;
-        let data2 = responseJSON.hourly.apparent_temperature;
-        let data3 = responseJSON.hourly.precipitation_probability;
-  
-        //Objeto de configuración del gráfico
-        let config = {
-          type: "line",
-          data: {
-            labels: labels,
-            datasets: [
-              {
-                label: "Relative humidity [2m]",
-                data: data1,
-              },
-              {
-                label: "apparent_temperature",
-                data: data2,
-              },
-              {
-                label: "precipitation_probability",
-                data: data3,
-              }
-            ],
-          },
-        };
-  
-        //Objeto con la instanciación del gráfico
-        let chart1 = new Chart(plotRef, config);
-      })
-      .catch(console.error);
-  };
+  //URL que responde con la respuesta a cargar
+  let URL =
+    "https://api.open-meteo.com/v1/forecast?latitude=-2.1962&longitude=-79.8862&hourly=relativehumidity_2m,apparent_temperature,precipitation_probability&timezone=auto";
 
+  fetch(URL)
+    .then((responseText) => responseText.json())
+    .then((responseJSON) => {
+      console.log(responseJSON);
+
+      //Respuesta en formato JSON
+      //Referencia al elemento con el identificador plot
+      let plotRef = document.getElementById("plot2");
+
+      //Etiquetas del gráfico
+      let labels = responseJSON.hourly.time;
+
+      //Etiquetas de los datos
+      let data1 = responseJSON.hourly.relativehumidity_2m;
+      let data2 = responseJSON.hourly.apparent_temperature;
+      let data3 = responseJSON.hourly.precipitation_probability;
+
+      //Objeto de configuración del gráfico
+      let config = {
+        type: "line",
+        data: {
+          labels: labels,
+          datasets: [
+            {
+              label: "Relative humidity [2m]",
+              data: data1,
+            },
+            {
+              label: "apparent_temperature",
+              data: data2,
+            },
+            {
+              label: "precipitation_probability",
+              data: data3,
+            },
+          ],
+        },
+      };
+
+      //Objeto con la instanciación del gráfico
+      let chart1 = new Chart(plotRef, config);
+    })
+    .catch(console.error);
+};
+
+let parseXML = (responseText) => {
+  const parser = new DOMParser();
+  const xml = parser.parseFromString(responseText, "application/xml");
+
+  // Referencia al elemento `#forecastbody` del documento HTML
+
+  let forecastElement = document.querySelector("#forecastbody");
+  forecastElement.innerHTML = "";
+
+  // Procesamiento de los elementos con etiqueta `<time>` del objeto xml
+  let timeArr = xml.querySelectorAll("time");
+
+  timeArr.forEach((time) => {
+    let from = time.getAttribute("from").replace("T", " ");
+
+    let humidity = time.querySelector("humidity").getAttribute("value");
+    let windSpeed = time.querySelector("windSpeed").getAttribute("mps");
+    let precipitation = time
+      .querySelector("precipitation")
+      .getAttribute("probability");
+    let pressure = time.querySelector("pressure").getAttribute("value");
+    let cloud = time.querySelector("clouds").getAttribute("all");
+
+    let template = `
+          <tr>
+              <td>${from}</td>
+              <td>${humidity}</td>
+              <td>${windSpeed}</td>
+              <td>${precipitation}</td>
+              <td>${pressure}</td>
+              <td>${cloud}</td>
+          </tr>
+      `;
+
+    //Renderizando la plantilla en el elemento HTML
+    forecastElement.innerHTML += template;
+  });
+};
+
+// Callback async
+let selectListener = async (event) => {
+  let selectedCity = event.target.value;
+  let cityStorage = localStorage.getItem(selectedCity);
+  if (cityStorage == null) {
+    try {
+      //API key
+      let APIkey = "15c22b52f3d8d6c4b70d75e8eeaba391";
+      let url = `https://api.openweathermap.org/data/2.5/forecast?q=${selectedCity}&mode=xml&appid=${APIkey}`;
+
+      let response = await fetch(url);
+      let responseText = await response.text();
+      await parseXML(cityStorage);
+      await localStorage.setItem(
+        selectedCity,
+        JSON.stringify({ data: responseText, timestamp: new Date().getTime() })
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    let storedData = JSON.parse(cityStorage);
+    let storedTimestamp = storedData.timestamp;
+    let currentTimestamp = new Date().getTime();
+    if (currentTimestamp - storedTimestamp > 3 * 60 * 60 * 1000) {
+      console.log("ya ha pasado 3 horas")
+      localStorage.removeItem(selectedCity);
+    } else {
+      console.log("no ha pasado 3 horas")
+      parseXML(storedData.data);
+    }
+  }
+};
+
+let loadForecastByCity = () => {
+  //Handling event
+  let selectElement = document.querySelector("select");
+  selectElement.addEventListener("change", selectListener);
+};
+
+loadForecastByCity();
 cargarPrecipitacion();
 cargarFechaActual();
 cargarOpenMeteo();
